e2df50cc13d57270d84d803f94cfe11b97850faf
Marijan Vukcevich <marijanvukcevich@Marijans-MacBook-Pro.local>
2013-08-06 09:52:04 -0700

    Initial Commit

---
+++ ThreeDecimalLimit/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ThreeDecimalLimit
+//
+//  Created by Marijan Vukcevich on 8/6/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ThreeDecimalLimit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ThreeDecimalLimit
+//
+//  Created by Marijan Vukcevich on 8/6/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
